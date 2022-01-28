7066bedff8ee70bef6d9c581f5fc8bad1a4a9651
Henry <henry.onyango@outlook.com>
2022-01-28 22:33:44 +0300

    Add mongodb and schema

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // Imports
+require("dotenv").config();
 const express = require("express");
-
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const res = require("express/lib/response");
+const mongo = require("mongodb");
+const mongoose = require("mongoose");
 const port = 3000;
 
 // Base config
@@ -14,13 +15,60 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// Db set up
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const { Schema, model } = mongoose;
+
+// todo: refactor so that these are from functions instead
+const exerciseSchema = new Schema({
+  username: { type: String, required: true },
+  description: { type: String, required: true },
+  duration: { type: Number, required: true },
+  date: { type: String, required: true },
+});
+
+const userSchema = new Schema({
+  username: { type: String, required: true },
+});
+
+const logSchema = new Schema({
+  username: { type: String, required: true },
+  count: Number,
+  log: [{ type: Schema.Types.ObjectId, ref: "Exercise" }],
+});
+
+const exerciseModel = model("Exercise", exerciseSchema);
+const userModel = model("User", userSchema);
+const logModel = model("Log", logSchema);
+
 // Routes
 app.get("/", (request, response) => {
   response.send("Hello world");
 });
+
+// User
 app.post("/api/users", (request, response) => {
   const { username } = request.body;
+
   response.json({ status: 200, user: username });
+});
+app.get("/api/users", (req, res) => {
+  res.send("Return all users");
+});
+
+// Exercise
+app.post("/api/:_id/exercises", (request, response) => {
+  // Todo: post exercise attached to a user
+});
+
+app.get("/api/users/:_id/logs", (req, res) => {
+  res.send(
+    "Returns an object with the count property of how many exercise belong to that user"
+  );
 });
 
 app.listen(port, () => {
